ad817a812e95e051be550b55a810718441b7f242
AshishAryaGithub <107829713+AshishAryaGithub@users.noreply.github.com>
2025-11-19 21:18:55 +0530

    Changed menu options if the user is logged-in

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { CommonModule, NgSwitch } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgSwitch, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  menuList:string = "default";
 
+  constructor(private _router: Router){}
+
+  ngOnInit(): void {
+    this._router.events.subscribe((val:any)=>{
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes("seller")){
+          this.menuList = "seller";
+        }
+        else{
+          this.menuList = "default";
+        }
+      }
+    })
+  }
+
+  logout(){
+    localStorage.removeItem('seller')
+    this._router.navigate(["/"])
+  }
 }
